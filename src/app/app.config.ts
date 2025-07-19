ba47d7e554ab34f0046c801dea77785d9928ffce
Alexandre <alex.silva250@hotmail.com>
2025-07-18 22:59:14 -0300

    feat: add e configuration on http interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,29 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig, inject,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {provideHttpClient} from '@angular/common/http';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {KeycloakService} from './utils/keycloak/keycloak';
+import {keycloakHttpInterceptor} from './utils/http/keycloak-http-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(
+      withInterceptors([keycloakHttpInterceptor])
+    ),
+    provideAppInitializer(()=>{
+      const initFn = ((key: KeycloakService)=>{
+        return () => key.init();
+      })(inject(KeycloakService));
+      return initFn();
+    })
   ]
 };
